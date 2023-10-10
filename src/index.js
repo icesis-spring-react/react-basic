bf99990721dc995fdb39a51c6ac298feaeabfcd3
John Vargas <javargas@gmail.com>
2023-10-09 20:03:17 -0500

    Lista de Estudiantes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import Layout from "./pages/Layout";
+import NoPage from "./pages/NoPage";
+import Students from "./pages/Students";
+
+export default function App() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="students" element={<Students />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
